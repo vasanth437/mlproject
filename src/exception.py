677e40458233f0a1ae6d61f484ce51ecb823708e
vasanth437 <nanivasanth437@gmail.com>
2023-08-15 17:30:48 +0530

    Logging and Exception

--- src/exception.py
+++ src/exception.py
@@ -18,3 +18,11 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+
+
+if __name__== "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("divide by zero")
+        raise CustomException(e,sys)
